975da2c969acaf2016bcbca3df2dbc9df5a76acf
happ-2h <hikonetgames@gmail.com>
2024-12-11 16:28:01 -0800

    Mobile entity class created

---
+++ src/entity/mobile/Entity_Mob.js
@@ -0,0 +1,13 @@
+import Entity from "../Entity";
+
+export default class Entity_Mob extends Entity {
+  constructor(x=0, y=0) {
+    super(x, y);
+  }
+
+  init() {}
+
+  update(dt) {}
+
+  draw() {}
+};
