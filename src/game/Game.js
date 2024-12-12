0bd82c68372e740ecc716fd6cbf8ea62a0e13a0c
happ-2h <hikonetgames@gmail.com>
2024-12-11 16:06:46 -0800

    Set up HTML5 canvas

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,7 +1,16 @@
+import { CANVAS_HEIGHT, CANVAS_WIDTH } from "./constants";
+
 export default class Game {
+  #cnv;  // HTML canvas reference
   #last; // Previous RAF timestamp
 
   constructor() {
+    // Init canvas
+    this.#cnv = document.querySelector("canvas");
+    this.#cnv.width  = CANVAS_WIDTH;
+    this.#cnv.height = CANVAS_HEIGHT;
+    this.#cnv.autofocus = true;
+
     this.#last = 0;
 
     this.init();
---
+++ src/game/constants.js
@@ -0,0 +1,11 @@
+// Game screen
+export const SCREEN_WIDTH  = 256;
+export const SCREEN_HEIGHT = 224;
+
+// Game constants
+export const SCALE = 3;    // Drawing scale
+export const DEBUG = true; // Show debug data
+
+// Canvas screen
+export const CANVAS_WIDTH  = SCREEN_WIDTH  * SCALE;
+export const CANVAS_HEIGHT = SCREEN_HEIGHT * SCALE;
