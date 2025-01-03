b0a411fbb41ff83c16c583752f563863e6e94c10
happ-2h <hikonetgames@gmail.com>
2025-01-03 12:20:37 -0800

    Camera speed relative to player

--- src/camera/Camera.js
+++ src/camera/Camera.js
@@ -1,4 +1,5 @@
-import { SCREEN_HEIGHT_TILES, SCREEN_WIDTH_TILES } from "../game/constants";
+import { SCREEN_HEIGHT_TILES, SCREEN_WIDTH_TILES, TILE_SIZE } from "../game/constants";
+import { lerp } from "../math/utils";
 import Vec2D from "../math/Vec2D";
 import Rectangle from "../utils/Rectangle";
 
@@ -6,13 +7,22 @@ export default class Camera {
   #rect;    // Camera's x, y, width, height
   #focalPt; // Camera's (x, y) focal point
 
+  #lerpSpeed;
+  #triggerDistY; // Location when the camera starts moving
+
   constructor(x=0, y=0, map=null) {
     this.#rect    = new Rectangle(x, y, SCREEN_WIDTH_TILES, SCREEN_HEIGHT_TILES);
     this.#focalPt = new Vec2D(x, y);
+
+    this.#lerpSpeed = 0.16;
+    this.#triggerDistY = 11 * TILE_SIZE;
   }
 
   focus(x, y) {
-    this.#focalPt.set(x, y);
+    this.#focalPt.set(
+      Math.floor(x),
+      Math.floor(y)
+    );
   }
 
   vfocus(vec) {
@@ -20,9 +30,13 @@ export default class Camera {
   }
 
   update(dt) {
-    // Move up if focused object is above 11 tiles in viewspace
-    if (this.#focalPt.y <= this.#rect.y + (11<<4))
-      --this.#rect.y;
+    // Move up if focused object is on or above trigger distance
+    if (this.#focalPt.y <= this.#rect.y + this.#triggerDistY)
+      this.#rect.y = lerp(
+        this.#rect.y,
+        this.#focalPt.y - this.#triggerDistY,
+        this.#lerpSpeed
+      );
 
     if (this.#rect.y <= 0) this.#rect.y = 0;
   }
@@ -34,7 +48,13 @@ export default class Camera {
   get width()  { return this.rect.dim.x; }
   get height() { return this.rect.dim.y; }
 
+  get lerpSpeed()    { return this.#lerpSpeed; }
+  get triggerDistY() { return this.#triggerDistY}
+
   // Mutators
   set x(x)          { this.#rect.x = x; }
   set y(y)          { this.#rect.y = y; }
+
+  set lerpSpeed(ls)   { this.#lerpSpeed = ls; }
+  set triggerDistY(y) { this.#triggerDistY = y; }
 };
